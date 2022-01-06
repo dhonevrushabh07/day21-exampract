1718999969a1c469066bf2c5804bcc08e2330ce4
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-06 19:23:33 +0530

    b1 simple form without db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent />
     </div>
   );
 }
 
-export default App;
+function MyComponent() {
+  const [username, setUsername] = useState("");
+  const [mobno, setMobno] = useState("gdg");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [list, setList] = useState([]);
+  const usernameHandler = (e) => {
+    const newusername = e.target.value;
+    setUsername(newusername);
+  };
+  const mobnoHandler = (e) => {
+    const newmobno = e.target.value;
+    setMobno(newmobno);
+  };
+  const emailHandler = (e) => {
+    const newemail = e.target.value;
+    setEmail(newemail);
+  };
+  const passwordHandler = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+  };
+  const register = () => {
+    //alert(`${username} ${mobno} ${email} ${password}`);
+
+    const user = {
+      username: username,
+      mobno: mobno,
+      email: email,
+      password: password,
+    };
+    const newlist = [user, ...list];
+    setList(newlist);
+    setUsername("");
+    setMobno("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const css1 = "form form-control w-50 bg-alert alert-success mb-2";
+
+  return (
+    <div className="bg-alert alert-danger">
+      <div className="bg-dark text-light p-2 fs-1 mb-1">Registration Form</div>
+      <div>
+        <input
+          type="text"
+          className={css1}
+          value={username}
+          placeholder="Enter username.."
+          onChange={usernameHandler}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          className={css1}
+          value={mobno}
+          placeholder="Enter mobile number.."
+          onChange={mobnoHandler}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          className={css1}
+          value={email}
+          placeholder="Enter email-id.."
+          onChange={emailHandler}
+        />
+      </div>{" "}
+      <div>
+        <input
+          type="text"
+          className={css1}
+          value={password}
+          placeholder="Enter password.."
+          onChange={passwordHandler}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          className="btn btn-outline-danger w-25 mb-1"
+          value="Register"
+          onClick={register}
+        />
+      </div>
+      <div>
+        {list.map((item) => (
+          <div className="bg-alert alert-info mb-1 p-1 border border-danger text-dark fw-bold">
+            `Name : {item.username}, Mob No: {item.mobno}, Email Id:{" "}
+            {item.email}, Password : {item.password}`
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
